c9e48fe00e8d73576cc70c7473b1107d0eeebcec
Ashutosh K <ashuthoshsk@outlook.com>
2023-05-14 22:11:21 +0530

    Some files

---
+++ organization-service/src/main/java/com/spma/organizationservice/model/Organization.java
@@ -0,0 +1,33 @@
+package com.spma.organizationservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "organizations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Organization {
+
+    @Id
+    @Column(name = "organization_id", nullable = false)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "contact_name", nullable = false)
+    private String contactName;
+
+    @Column(name = "contact_email", nullable = false)
+    private String contactEmail;
+
+    @Column(name = "contact_phone", nullable = false)
+    private String contactPhone;
+}
